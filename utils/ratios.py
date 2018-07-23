447e682aebf967a4bcefadbf472fe9e51cc763b2
Julio Dominguez <jcdoming@ucsd.edu>
2018-07-23 00:02:21 -0700

    added new ratio method, util/ratios now has all methods and a method to run them all. Simulate different slopes and save results. Analyze results through R

--- utils/ratios.py
+++ utils/ratios.py
@@ -55,3 +55,118 @@ calc_alpha_beta_ratio = partial(calc_band_ratio, low_band_range=ALPHA_BAND, high
 calc_theta_beta_ratio.__doc__ = calc_band_ratio.__doc__
 calc_theta_alpha_ratio.__doc__ = calc_band_ratio.__doc__
 calc_alpha_beta_ratio.__doc__ = calc_band_ratio.__doc__
+
+    
+def diff_CF_Power_only(treatment,control,low_band,high_band):
+    """Calculate band ratio by finding
+    ----------
+    treatment : fooof object/ fooof.group object
+        fooof'ed power spectrum
+    control : fooof object/ fooof.group object
+        fooof'ed power spectrum
+    
+
+    Outputs
+    -------
+    ratio : float
+        Oscillation power ratio.
+    """
+    #TODO: Assumes one peak in each band
+    #Gather peak info for both PSDs
+    treatment_peaks = treatment.get_all_data('peak_params')
+    control_peaks = control.get_all_data('peak_params')
+
+    treatmentRatioList = []
+    controlRatioList = []
+
+    low_found = False
+    #curr_id
+    
+    # adds the power ratio of each peak contained in low_band and high band respectively to a list 
+    for i in range(len(treatment_peaks)):
+        if((low_band[0] <= treatment_peaks[i][0] <=low_band[1]) and low_found == False):
+            low_found = True
+            #curr_id = treatment_peaks[i][3]
+            low_power = treatment_peaks[i][1]
+            continue
+
+        elif((low_found == True) and high_band[0]<=treatment_peaks[i][0]<=high_band[1]):
+            high_power = treatment_peaks[i][1]
+            treatmentRatioList.append(low_power/high_power)
+            low_found = False
+            
+    for i in range(len(control_peaks)):
+        if((low_band[0] <= control_peaks[i][0] <=low_band[1]) and low_found == False):
+            low_found = True
+            #curr_id = treatment_peaks[i][3]
+            low_power = control_peaks[i][1]
+            continue
+
+        elif((low_found == True) and high_band[0]<=control_peaks[i][0]<=high_band[1]):
+            high_power = control_peaks[i][1]
+            controlRatioList.append(low_power/high_power)
+            low_found = False
+
+    res = abs(np.mean(treatmentRatioList)-np.mean(controlRatioList))
+    print("Diff CF only:" ,res)
+    return res
+
+def diff_average_power_set_band(treatment,control,low_band,high_band):
+    """Calculate band ratio by dividing the power of the low and high central frequencies
+    ----------
+    treatment : fooof object/ fooof.group object
+        fooof'ed power spectrum
+    control : fooof object/ fooof.group object
+        fooof'ed power spectrum
+    
+
+    Outputs
+    -------
+    ratio : float
+        Oscillation power ratio.
+    """
+
+    treatment_ratio = calc_band_ratio(treatment.freqs,treatment.power_spectra,low_band,high_band)
+    control_ratio = calc_band_ratio(control.freqs, control.power_spectra,low_band,high_band)
+
+    res = abs(treatment_ratio-control_ratio)
+    print("Diff ratio of average power set bands: ", res)
+    return res
+
+
+def diff_sum_div_band(treatment, control,low_band,high_band):
+    
+
+    # Extract frequencies within each specified band
+    _, low_band_powers = trim_spectrum(treatment.freqs, treatment.power_spectra, low_band)
+    _, high_band_powers = trim_spectrum(control.freqs, control.power_spectra,high_band)
+   
+    # Divides each sum of each power spectrum by bandwidth.
+    low_sum = sum(low_band_powers) / len(low_band) 
+    high_sum = sum(high_band_powers) / len(low_band)
+    
+    # Average divided sums and find ratio
+    res = np.mean(low_sum)/np.mean(high_sum)
+    print("Diff sum(power in band)/bandwidth: ",res)
+    return res
+
+#By this point fooof should have fit both treatment and control
+def ratios(treatment,control,low_band=[4,8],high_band=[15,30]):
+
+    res=[]
+    # Execute first method:
+    # difference in power of central frequency 
+    # of high and low frequency peak only
+    res.append(diff_CF_Power_only(treatment,control,low_band,high_band))
+    print("\t")
+
+    # Execute second method:
+    # difference in average power in set bands ratio
+    res.append(diff_average_power_set_band(treatment,control,low_band,high_band))
+    print("\t")
+
+    # Execute third method:
+    # difference in sum(powers in set band)/bandwidth ratio
+    res.append(diff_sum_div_band(treatment,control,low_band,high_band))
+    
+    return res
