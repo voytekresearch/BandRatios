c2238fe076964cfb0e4ea6f924759ac40ec6ce05
Julio Dominguez <juliodominguez7@gmail.com>
2019-09-03 11:12:41 -0700

    fixed interacting sim tick labels

--- scripts/gen_interacting_plot.py
+++ scripts/gen_interacting_plot.py
@@ -9,7 +9,7 @@ from plot import *
 def main():
     
     #Create figure for paper
-    plt.figure()
+    plt.figure(figsize=(14,12))
 
     #Import data
     exp_lowcf_data = np.load('../dat/interacting_param_sims/exp_lowcf_data.npy')
@@ -19,15 +19,23 @@ def main():
 
     ax1 = plt.subplot(221)
     ax1 = plot_interacting_sims_paper(calc_interacting_param_ratios(exp_lowcf_data),  CFS_LOW, EXPS,ax=ax1)
+    plt.xticks(rotation=45, horizontalalignment='right');
+    plt.yticks(rotation=0)
     
     ax2 = plt.subplot(222)
     ax2 = plot_interacting_sims_paper(calc_interacting_param_ratios(lowpw_lowbw_data), BWS, PWS ,ax=ax2)
+    plt.xticks(rotation=45, horizontalalignment='right');
+    plt.yticks(rotation=0)
     
     ax3 = plt.subplot(223)
     ax3 = plot_interacting_sims_paper(calc_interacting_param_ratios(lowcf_highbw_data), BWS, CFS_LOW,ax=ax3)
+    plt.xticks(rotation=45, horizontalalignment='right');
+    plt.yticks(rotation=0)
     
     ax4 = plt.subplot(224)
     ax4 = plot_interacting_sims_paper(calc_interacting_param_ratios(highcf_highpw_data), PWS, CFS_HIGH ,ax=ax4)   
+    plt.xticks(rotation=45, horizontalalignment='right');
+    plt.yticks(rotation=0)
     
     plt.savefig("../figures/InteractingSims/interacting_params_paper_fig",dpi=400)
 
